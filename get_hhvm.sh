2a04f6e15fa27d3926f3142c96862fe4548351cb
Zi Shen Lim <zlim.lnx@gmail.com>
2016-07-14 07:27:14 +0000

    get_hhvm: even if cherry-pick fails, continue

--- get_hhvm.sh
+++ get_hhvm.sh
@@ -4,19 +4,19 @@ git clone --depth 1 https://github.com/facebook/hhvm.git ${HHVM_PATH}
 pushd ${HHVM_PATH}
 git checkout -b arm64
 git fetch origin pull/7206/head:sunil-sk-pr7206
-git cherry-pick sunil-sk-pr7206
+git cherry-pick sunil-sk-pr7206 || git cherry-pick --abort
 git fetch origin pull/7218/head:lpathy-pr7218
-git cherry-pick lpathy-pr7218
+git cherry-pick lpathy-pr7218 || git cherry-pick --abort
 git submodule update --init --recursive
 pushd third-party/folly/src/
 git checkout -b arm64
 git fetch origin pull/429/head:lpathy-pr429
-git cherry-pick lpathy-pr429
+git cherry-pick lpathy-pr429 || git cherry-pick --abort
 git fetch origin pull/430/head:zhangkehong2brad-pr430
-git cherry-pick zhangkehong2brad-pr430
+git cherry-pick zhangkehong2brad-pr430 || git cherry-pick --abort
 popd # third-party/folly/src/
 pushd third-party/mcrouter/src/
 git checkout -b arm64
 git fetch origin pull/134/head:zhangkehong2brad-pr134
-git cherry-pick zhangkehong2brad-pr134
+git cherry-pick zhangkehong2brad-pr134 || git cherry-pick --abort
 popd # third-party/mcrouter/src/
