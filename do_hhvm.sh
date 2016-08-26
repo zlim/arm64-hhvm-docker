61bd35314bfa13c3edcb88df44b00072e3a70935
Zi Shen Lim <zlim.lnx@gmail.com>
2016-08-26 06:15:32 +0000

    do_hhvm: build/test Release, then Debug

--- do_hhvm.sh
+++ do_hhvm.sh
@@ -8,6 +8,8 @@ mkdir -p ${LOGS_PATH}
 
 HHVM_PATH=/git/hhvm-${HHVM_REV}
 HHVM_LOG=${LOGS_PATH}/make-hhvm-${HHVM_REV}.log
+HHVM_DEBUG_LOG=${LOGS_PATH}/make-hhvm-debug-${HHVM_REV}.log
 
 make HHVM_PATH=${HHVM_PATH} get summary hhvm test 2>&1 | tee ${HHVM_LOG}
+make HHVM_PATH=${HHVM_PATH} CMAKE_BUILD_TYPE=Debug clean summary hhvm test 2>&1 | tee ${HHVM_DEBUG_LOG}
 
