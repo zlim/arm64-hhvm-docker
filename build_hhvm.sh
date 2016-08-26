6a54a3f6736195d93ae834c684e83d5e81977047
Zi Shen Lim <zlim.lnx@gmail.com>
2016-08-26 05:48:31 +0000

    build_hhvm: ENABLE_SPLIT_DWARF, Release build by default

--- build_hhvm.sh
+++ build_hhvm.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Release}
 MAKE="make -j `nproc`"
+EXTRA_CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DENABLE_SPLIT_DWARF=True"
 echo "========== `date`"
-cmake -DMYSQL_UNIX_SOCK_ADDR=/var/run/mysqld/mysqld.sock .
+cmake ${EXTRA_CMAKE_FLAGS} -DMYSQL_UNIX_SOCK_ADDR=/var/run/mysqld/mysqld.sock .
 echo "========== `date`"
 ${MAKE}
 echo "========== `date`"
