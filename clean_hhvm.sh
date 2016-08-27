607633edc0934c33c69ed7b3d4a0281387e017b3
Zi Shen Lim <zlim.lnx@gmail.com>
2016-08-27 19:34:00 +0000

    clean_hhvm: fix up

--- clean_hhvm.sh
+++ clean_hhvm.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+HHVM_PATH=${HHVM_PATH:-/git/hhvm}
+pushd ${HHVM_PATH}
 echo "========== `date`"
 make clean
 echo "========== `date`"
-find $(HHVM_PATH) -name CMakeCache.txt -delete
+find . -name CMakeCache.txt -delete
 echo "========== `date`"
+popd # ${HHVM_PATH}
