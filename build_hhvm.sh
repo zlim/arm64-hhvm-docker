d65fb2949b695b0c9d1867ef8982e12e2540f174
Zi Shen Lim <zlim.lnx@gmail.com>
2016-08-27 19:36:19 +0000

    build_hhvm: build within HHVM_PATH

--- build_hhvm.sh
+++ build_hhvm.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+HHVM_PATH=${HHVM_PATH:-/git/hhvm}
 CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Release}
 MAKE="make -j `nproc`"
 EXTRA_CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DENABLE_SPLIT_DWARF=True"
+pushd ${HHVM_PATH}
 echo "========== `date`"
 cmake ${EXTRA_CMAKE_FLAGS} -DMYSQL_UNIX_SOCK_ADDR=/var/run/mysqld/mysqld.sock .
 echo "========== `date`"
 ${MAKE}
 echo "========== `date`"
+popd # ${HHVM_PATH}
