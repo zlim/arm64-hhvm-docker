58f711f65e8926e13943de0eba36fb2a9a0908c7
Zi Shen Lim <zlim.lnx@gmail.com>
2016-08-27 19:37:55 +0000

    do_hhvm: use -debug prefix for log
    This helps with file listing whether it's by name or time.

--- do_hhvm.sh
+++ do_hhvm.sh
@@ -8,7 +8,7 @@ mkdir -p ${LOGS_PATH}
 
 HHVM_PATH=/git/hhvm-${HHVM_REV}
 HHVM_LOG=${LOGS_PATH}/make-hhvm-${HHVM_REV}.log
-HHVM_DEBUG_LOG=${LOGS_PATH}/make-hhvm-debug-${HHVM_REV}.log
+HHVM_DEBUG_LOG=${LOGS_PATH}/make-hhvm-${HHVM_REV}-debug.log
 HPHP_TEST=all
 
 make HHVM_PATH=${HHVM_PATH} HPHP_TEST=${HPHP_TEST} \
